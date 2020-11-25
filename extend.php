9abd9582bc017b652e26e170c8419a12bf04adae
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2020-11-25 15:38:56 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -8,19 +8,15 @@
  */
 
 use Flarum\Extend;
-use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->content(function (Document $document) {
-            $document->head[] = '<script src="../assets/extensions/zerosonesfun-lolight/lolight-1.4.0.min.js"></script>';
-        }),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
                 '[syntax]{TEXT}[/syntax]',
-                '<div class="lolight">{TEXT}</div>'
+                '<pre class="lolight">{TEXT}</pre>
+                 <script src="../assets/extensions/zerosonesfun-lolight/lolight-1.4.0.min.js"></script>'
             );
         })
 ];
